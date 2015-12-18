61d21d5fb63d89ec50741ed7d685dc3cefce955f
Netznarkose <mmaintz@posteo.de>
2015-12-18 19:12:19 +0100

    users_controller_spec.rb updated and refactored & users_controller.rb refactored
    users_controller_spec.rb refactoring2 test :33 & :42
    
    users_controller_spec.rb refactoring3 test :50
    
    users_controller_spec.rb refactoring4
    
    users_controller_spec.rb refactoring5 specs passes rubocop-test now
    
    Add Gitter badge
    
    issue  #11 misleading message - only admins are redirected to show (edit role form)
    
    issue #11 / uniq added for error messages on update of email-address
    
    emailupdate & errormessage (issue #11) now covered
    
    jose tipps and tricks
    
    jose session recapitulation
    
    go refactoring users_controller_specs.rb
    
    refactoring users_controller_spec.rb covering every action now
    
    refactoring and fixing test
    
    go on
    
    refactoring controllers/users_controller_spec.rb and fatories.rb
    
    last commits with comments in test
    
    refactor1
    
    refactor2
    
    refactor delete method
    
    refactorxx
    
    refactoring with martin1
    
    refactorfffff
    
    refactor json
    
    remove json testing prepare for pull-request
    
    users_controller_spec.rb updated and refactored & users_controller.rb refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,103 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :update_role, :update]
+  before_filter :protect_from_non_admins, except: [:edit, :update]
+  before_filter :protect_from_non_currents, only: [:edit, :update]
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
-
-    if admin?
-      respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json: @users }
-      end
-    else
-      flash[:notice] = 'Access denied!'
-      redirect_to root_path
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
     end
-
   end
 
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'Mitarbeiter erfolgreich erstellt!' }
+        format.json { render json: @user, role: :created, location: @user }
+      else
+        format.html { redirect_to new_user_path, notice: @user.errors.messages.values.flatten.uniq.join('<br/>') }
+        format.json { render json: @user.errors, role: :unprocessable_entity } 
+      end
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    respond_to do |format|
+      format.html # edit.html.erb
+      # no json why then in new?
+    end
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    redirect_path = admin? ? user_path(@user) : entries_path
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to redirect_path, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to edit_user_path(@user), notice: @user.errors.messages.values.flatten.uniq.join('<br />') }
+        format.json { render json: @user.errors, role: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update_role
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to user_path(@user), notice: 'Something went wrong.' }
+        format.json { render json: @user.errors, role: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    respond_to do |format|
+      @user = User.find(params[:id])
+      @user.destroy
+      format.html { redirect_to users_url, notice: "User #{@user.name} was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
+  # TODO move this method to entries_controller.rb
   def entries
     @page = params[:page] || 0
     params[:search] = nil if params[:search] and params[:search].strip == ""
-    all_entries = (params[:search] ? current_user.search_entries(params[:search]) : current_user.entries).order("romaji_order")
+    all_entries = (params[:search] ? current_user.search_entries(params[:search]) : current_user.entries).order('romaji_order')
     @count = all_entries.count
     @entries = all_entries.page(@page)
     respond_to do |format|
@@ -29,111 +106,34 @@ class UsersController < ApplicationController
     end
   end
 
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-    if admin?
-      respond_to do |format|
-        format.html # show.html.erb
-        format.json { render json: @user }
-      end
-    else
-      flash[:notice] = 'Access denied!'
-      redirect_to  root_path
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    if current_user.id == params[:id].to_i
-      @user = User.find(params[:id])
-      respond_to do |format|
-        format.html # edit.html.erb
-      end
-    else
-      flash[:notice] = 'Access denied!'
-      redirect_to root_path
-    end
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to users_path, notice: 'Mitarbeiter erfolgreich erstellt!' }
-        format.json { render json: @user, role: :created, location: @user }
-      else
-        format.html { redirect_to new_user_path, notice: @user.errors.messages.values.flatten.uniq.join("<br/>") }
-        format.json { render json: @user.errors, role: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-    if params[:user]["role"] && !admin?
-      params[:user].delete("role")
-    end
-
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_path(@user), notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to edit_user_path(@user), notice: "Something went wrong. #{@user.errors.messages.values.join('<br />')}" }
-        format.json { render json: @user.errors, role: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update_role
-    @user = User.find(params[:id])
-    respond_to do |format|
-      if admin?
-        if @user.update(user_params)
-          format.html { redirect_to users_path, notice: 'User was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { redirect_to user_path(@user), notice: 'Something went wrong.' }
-          format.json { render json: @user.errors, role: :unprocessable_entity }
-        end
-      else
-        format.html { redirect_to root_path, notice: 'Access denied!' }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User #{@user.name} was successfully deleted." }
-      format.json { head :no_content }
-    end
-  end
   private
-    def user_params
-      params.require(:user).permit(:name, :role, :password_confirmation, :password, :email)
+
+  def user_params
+    params.require(:user).permit(:name, :role, :password_confirmation, :password, :email)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def protect_from_non_admins
+    handle_unauthorized unless admin?
+  end
+
+  def protect_from_non_currents
+    @user = User.find(params[:id])
+    handle_unauthorized unless current_user.id == @user.id
+  end
+
+  def handle_unauthorized
+    unless admin?
+      respond_to do |format|
+        format.html do
+          flash[:notice] = 'Access denied!'
+          redirect_to root_path
+        end
+        format.json { render nothing: true, status: :unauthorized }
+      end
     end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,96 +1,202 @@
 require 'spec_helper'
 
-describe UsersController, :type => :controller do
-  describe "test if only admins can inspect the user list" do
-    it "should show user list to admin" do
-      admin = FactoryGirl.create(:admin)
-      sign_in admin
-      get :index
-      response.status.should == 200
-      sign_out admin
+describe UsersController, type: :controller do
+  let(:admin) { FactoryGirl.create(:admin) }
+  let(:editor) { FactoryGirl.create(:editor) }
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe 'get index' do
+    context 'as admin' do
+      it 'shows user index' do
+        sign_in admin
+        get :index
+        expect(response).to be_success
+      end
     end
-    it "should not show user list editor" do
-      editor = FactoryGirl.create(:editor)
-      sign_in editor
-      get :index
-      response.status.should == 302
-      sign_out editor
+    context 'as editor' do
+      it 'does not show user index' do
+        sign_in editor
+        get :index
+        expect(response).to be_redirect
+      end
     end
   end
 
-  describe "test if only admins can read users' information and update their role" do
-    before :each do
-      @admin = FactoryGirl.create(:admin)
-      @user = FactoryGirl.create(:editor)
+  describe 'get show' do
+    context 'as admin' do
+      it 'assigns the requested user to @user and renders the #show-view' do
+        sign_in admin
+        get :show, id: user.id
+        expect(assigns(:user)).to eq(user)
+        expect(response).to be_success
+        expect(response).to render_template :show
+      end
     end
-    it "should show user's information to admin" do
-      sign_in @admin
-      get :index, :id => 1
-      response.status.should == 200
-      sign_out @admin
-    end
-
-    it "should locate the requested user and update role for admin" do
-      sign_in @admin
-      put :update_role, :id => @user.id, user: FactoryGirl.attributes_for(:editor, role: "admin")
-      assigns(:user).should eq(@user)
-      @user.reload
-      @user.role.should == "admin"
-      sign_out @admin
-    end
-
-    it "should not update user's role for common user" do
-      sign_in @user
-      put :update_role, :id => @user.id, user: FactoryGirl.attributes_for(:editor, role: "admin")
-      @user.reload
-      @user.role.should_not == "admin"
-      sign_out @user
-    end
-
-    it "should update other attributes of current user's profile but no role" do
-      sign_in @user
-      put :update, :id => @user.id, :user => {"name" => "new_name", "email" => "new@email.com", "role" => "alien"}
-      @user.reload
-      @user.role.should_not == "alien"
-      @user.role.should == "editor"
-      @user.name.should == "new_name"
-      @user.email.should == "new@email.com"
-      sign_out @user
-    end
-
-  end
-
-  describe "test if current user can edit his own profile" do
-    before(:each) do
-      @user = FactoryGirl.create(:editor)
-      sign_in @user
-    end
-
-    it "should show edit page for current user's own profile" do
-      get :edit, :id => @user.id
-      response.should render_template("edit")
-    end
-
-    it "should not show edit page for another user's profile" do
-      other = FactoryGirl.create(:editor)
-      sign_in @user
-      get :edit, :id => other.id
-      response.should_not render_template("edit")
-      response.should redirect_to root_path
+    context 'as editor' do
+      it 'does not renders the #show-view and redirects to homepage' do
+        sign_in editor
+        get :show, id: user
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 
-  describe "test if can delete a user" do
-    before :each do
-      @user = FactoryGirl.create(:editor)
+  describe 'get new' do
+    context 'as admin' do
+      it 'assigns a new user to @User & renders #new-view' do
+        sign_in admin
+        get :new
+        expect(assigns(:user)).to be_a_new(User)
+        expect(response).to be_success
+        expect(response).to render_template :new
+      end
     end
-    it "should delete a user" do
-      pending
-      delete :destroy, :id => @user.id
-      User.find(@user.id).should be_nil
-      response.should redirect_to users_path
+    context 'as editor' do
+      it 'redirects to homepage & shows an error-message' do
+        sign_in editor
+        get :new
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).to eq('Access denied!')
+      end
     end
   end
 
+  describe 'POST create' do
+    context 'as admin' do
+      context 'with valid attributes' do
+        it 'creates a new contact, redirects to user-index
+        & shows a confirmation-message' do
+          sign_in admin
+          expect {
+            post :create, user: FactoryGirl.attributes_for(:user)
+          }.to change(User, :count).by(1)
+          expect(response).to redirect_to(users_path)
+          expect(flash[:notice]).to eq('Mitarbeiter erfolgreich erstellt!')
+        end
+      end
+      context 'with invalid attributes' do
+        it 'does not create a new contact, redirects to #new-view
+        & shows an error-message' do
+          sign_in admin
+          expect {
+            post :create, user: FactoryGirl.attributes_for(:editor, name: '')
+          }.to_not change(User, :count)
+          expect(response).to redirect_to(new_user_path)
+          expect(flash[:notice]).not_to be_empty
+        end
+      end
+    end
+    context 'as editor' do
+      it 'does not create a new contact, redirects to homepage
+      and shows an error-message' do
+        sign_in editor
+        expect {
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to_not change(User, :count)
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).not_to be_empty
+      end
+    end
+  end
 
+  describe 'get edit' do
+    context 'as current user' do
+      it 'I can render my #edit-view' do
+        sign_in editor
+        get :edit, id: editor.id
+        expect(response).to render_template(:edit)
+      end
+      it 'I get redirected to homepage when I try to render
+      someone elses #edit-view & get an error-message' do
+        sign_in editor
+        get :edit, id: user.id
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).to eq('Access denied!')
+      end
+    end
+  end
+
+  describe 'get update' do
+    context 'as current user' do
+      it 'I can update my name and email and get a confirmation-message' do
+        sign_in editor
+        put :update, id: editor.id, user: { name: 'different_editor name',
+                                            email: 'different_editor@user.com' }
+        editor.reload
+        expect(editor.name).to eq('different_editor name')
+        expect(editor.email).to eq('different_editor@user.com')
+        expect(flash[:notice]).to eq('User was successfully updated.')
+      end
+      it 'I get redirected to #edit-view when I make an Input failure
+      & get an error-message' do
+        sign_in editor
+        put :update, id: editor.id, user: { email: '' }
+        expect(response).to redirect_to edit_user_path(editor)
+        expect(flash[:notice]).to eq('Email darf nicht leer sein!')
+        put :update, id: editor.id, user: { name: '' }
+        expect(flash[:notice]).to eq('Name darf nicht leer sein!')
+      end
+      it 'I can not update my role' do
+        pending('role is currently managed in the update_role action')
+        sign_in editor
+        put :update, id: editor.id, user: { role: 'alien' }
+        editor.reload
+        expect(editor.role).not_to eq('alien')
+        expect(editor.role).to eq('editor')
+      end
+      it 'I can not update someone elses profile' do
+        sign_in editor
+        put :update, id: user.id, user: { name: 'different_user name' }
+        user.reload
+        expect(user.name).not_to eq('different_user name')
+      end
+    end
+  end
+
+  describe 'get update_role' do
+    context 'as admin' do
+      it 'i can make editors to admins' do
+        sign_in admin
+        put :update_role, id: editor.id, user: { role: 'admin' }
+        expect(assigns(:user)).to eq(editor)
+        editor.reload
+        expect(editor.role).to eq('admin')
+      end
+    end
+    context 'as editor' do
+      it 'i can not change any roles' do
+        sign_in editor
+        put :update_role, id: editor.id, user: { role: 'admin' }
+        editor.reload
+        expect(editor.role).not_to eq('admin')
+        expect(editor.role).to eq('editor')
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    before do
+      user
+    end
+    context 'as admin' do
+      it 'I can delete users and get redirected to users index' do
+        sign_in admin
+        expect{
+          delete :destroy, id: user.id
+        }.to change(User, :count).by(-1)
+        expect(response).to redirect_to(users_path)
+      end
+    end
+    context 'as editor' do
+      it 'I can not delete users, get redirected to homepage
+      and get an error-message' do
+        sign_in editor
+        expect{
+          delete :destroy, id: user.id
+        }.to change(User, :count).by(0)
+        expect(response).to redirect_to root_path
+        expect(flash[:notice]).to eq('Access denied!')
+      end
+    end
+  end
 end
