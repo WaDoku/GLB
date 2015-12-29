4abb3a4c9fd98bf06ad0bbcada7724a8170c4c2c
Netznarkose <mmaintz@posteo.de>
2015-12-29 17:11:31 +0100

    validate presence of name, email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @editor = FactoryGirl.create(:editor)
+  let(:editor) { FactoryGirl.create(:editor) }
+
+  before do
+    editor
   end
-  it "should create a new instance of a user given valid attributes" do
-    @editor.should be_persisted
-    @editor.role.should == "editor"
+  it "should create a new instance of a user
+  given valid attributes" do
+    expect(editor).to be_valid
+    expect(editor.role).to eq('editor')
+  end
+  it "is invalid without name" do
+    editor.name = nil
+    expect(editor).not_to be_valid
+  end
+  it "is invalid without email" do
+    editor.email= nil
+    expect(editor).not_to be_valid
+  end
+  it "is invalid without password" do
+    editor.password = nil
+    expect(editor).not_to be_valid
   end
 end
