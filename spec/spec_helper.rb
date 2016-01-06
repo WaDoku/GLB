c9f1034c3b897232002fd11cd8d10541282e1983
Iris Vogel <iris.vogel@uni-hamburg.de>
2016-01-06 21:32:00 +0100

    password request spec fixed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,4 +55,5 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
   config.include Devise::TestHelpers, :type => :view
   config.include Devise::TestHelpers, :type => :helper
+  config.include Rails.application.routes.url_helpers
 end
