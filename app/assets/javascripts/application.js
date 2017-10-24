bab4a6f31f6c554f8972f7c624752898ec3c80b5
max <mmaintz@posteo.de>
2017-10-24 11:03:23 +0200

    Task 2/5 Redesign Interface

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,69 +15,93 @@
 //= require turbolinks
 //= require ckeditor/init
 //= require_tree .
-
-var count = 0;
-var pages = ["page0", "page1", "page2", "page3", "page4", "page5", "page6", "page7", "page8"];
-var page_titles = ["oben", "mitte", "unten", "oben", "mitte", "unten", "oben", "mitte", "unten"];
-
-$(document).ready(function () {
-  set_page_number()
+  
+$(document).ready(function(){
+  $("#hide_lemma_schreibungen").click(function(){
+    $(".toggle_lemma_schreibungen_visibility").hide(400);
+  });
 });
 
-$(document).on("page:load", function() {
-  set_page_number()
-})
+$(document).ready(function(){
+  $("#show_lemma_schreibungen").click(function(){
+    $(".toggle_lemma_schreibungen_visibility").show(400);
+  });
+});
 
-function set_page_number() {
-  count = 0;
-  var default_page_number = document.getElementById("kennzahl").dataset.kennzahl;
-  document.getElementById("page_count").innerHTML = 'Seite ' + default_page_number + ', oben';
-}
+$(document).ready(function(){
+  $("#hide_uebersetzung").click(function(){
+    $(".toggle_uebersetzung_visibility").hide(400);
+  });
+});
 
-function back_first_switch() {
-  count = 0;
-  write_page_count();
-}
+$(document).ready(function(){
+  $("#show_uebersetzung").click(function(){
+    $(".toggle_uebersetzung_visibility").show(400);
+  });
+});
 
-function back_switch() {
-  if (count <= 0) {
-    count = 0;
-  } else {
-    count--;
-  }
-  write_page_count();
-}
+// var count = 0;
+// var pages = ["page0", "page1", "page2", "page3", "page4", "page5", "page6", "page7", "page8"];
+// var page_titles = ["oben", "mitte", "unten", "oben", "mitte", "unten", "oben", "mitte", "unten"];
 
-function forward_switch() {
-  if (count >= 8) {
-    count = 8;
-  } else {
-    count++;
-  }
-  write_page_count();
-}
+// $(document).ready(function () {
+//   set_page_number()
+// });
 
-function write_page_count() {
+// $(document).on("page:load", function() {
+//   set_page_number()
+// })
 
-  var page_number_as_string = document.getElementById("kennzahl").dataset.kennzahl;
-  var page_number_as_integer = Math.floor(page_number_as_string);       
-  if (count > 2) {
-    console.log('else 1')
-      page_number_as_integer += 1;
-  }
-  if (count > 5) {
-    console.log('else 2')
-      page_number_as_integer += 1;
-  } 
-  document.getElementById("page_count").innerHTML = 'Seite ' + page_number_as_integer.toString() + ", " + page_titles[count];
+// function set_page_number() {
+//   count = 0;
+//   var default_page_number = document.getElementById("kennzahl").dataset.kennzahl;
+//   document.getElementById("page_count").innerHTML = 'Seite ' + default_page_number + ', oben';
+// }
+
+// function back_first_switch() {
+//   count = 0;
+//   write_page_count();
+// }
+
+// function back_switch() {
+//   if (count <= 0) {
+//     count = 0;
+//   } else {
+//     count--;
+//   }
+//   write_page_count();
+// }
+
+// function forward_switch() {
+//   if (count >= 8) {
+//     count = 8;
+//   } else {
+//     count++;
+//   }
+//   write_page_count();
+// }
+
+// function write_page_count() {
+
+//   var page_number_as_string = document.getElementById("kennzahl").dataset.kennzahl;
+//   var page_number_as_integer = Math.floor(page_number_as_string);       
+//   if (count > 2) {
+//     console.log('else 1')
+//       page_number_as_integer += 1;
+//   }
+//   if (count > 5) {
+//     console.log('else 2')
+//       page_number_as_integer += 1;
+//   } 
+//   document.getElementById("page_count").innerHTML = 'Seite ' + page_number_as_integer.toString() + ", " + page_titles[count];
 
 
-  for (var i = 0; i < 9; i++) {
-    if (count == i) {
-      document.getElementById(pages[i]).style.display = "block";
-    } else {
-      document.getElementById(pages[i]).style.display = "none";
-    }
-  }
+//   for (var i = 0; i < 9; i++) {
+//     if (count == i) {
+//       document.getElementById(pages[i]).style.display = "block";
+//     } else {
+//       document.getElementById(pages[i]).style.display = "none";
+//     }
+//   }
 
-}
+// }
