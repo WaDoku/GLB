115e0c94478963f2babbf09293716fec042a1428
max <mmaintz@posteo.de>
2017-11-06 14:33:59 +0100

    refactor entry#index action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,19 +6,14 @@ class EntriesController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    if params[:search]
-      @entries = Entry.search(params[:search]).page(params[:page])
-      @count = Entry.search(params[:search]).count # refactoring needed
-    else
-      @entries = Entry.order(sort_column + " " + sort_direction).page(params[:page])
-      @count = Entry.order(sort_column + " " + sort_direction).count # refactoring needed
-    end
+    @entries = params[:search] ? search_entries : sort_entries
+    @count = all_entries.count
     respond_to do |format|
       format.html
       format.json { render json: all_entries }
-      format.xml  { send_data all_entries.to_xml, :type => 'text/xml', :disposition => "attachment; filename=glb.xml" }
-      format.text { send_data customized_xml(all_entries), :type => 'text/xml', :disposition => "attachment; filename=customized_glb.xml" }
-      format.csv  { send_data customized_csv(all_entries), :type => 'text/csv', :disposition => "attachment; filename=glb.csv" }
+      format.xml  { send_data all_entries.to_xml, type: 'text/xml', disposition: 'attachment; filename=glb.xml' }
+      format.text { send_data customized_xml(all_entries), type: 'text/xml', disposition: 'attachment; filename=customized_glb.xml' }
+      format.csv  { send_data customized_csv(all_entries), type: 'text/csv', disposition: 'attachment; filename=glb.csv' }
     end
   end
 
@@ -43,7 +38,7 @@ class EntriesController < ApplicationController
         format.html { redirect_to @entry, notice: 'Eintrag erfolgreich erstellt.' }
         format.json { render json: @entry, status: :created, location: @entry }
       else
-        format.html { render action: "new" }
+        format.html { render action: 'new' }
         format.json { render json: @entry.errors, status: :unprocessable_entity }
       end
     end
@@ -55,7 +50,7 @@ class EntriesController < ApplicationController
         format.html { redirect_to @entry, notice: "Eintrag erfolgreich editiert. #{undo_link}" }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { render action: 'edit' }
         format.json { render json: @entry.errors, status: :unprocessable_entity }
       end
     end
@@ -92,17 +87,27 @@ class EntriesController < ApplicationController
   end
 
   def undo_link
-    view_context.link_to("Rückgängig", revert_version_path(@entry.versions.reload.last), :method => :post)
+    view_context.link_to('Rückgängig', revert_version_path(@entry.versions.reload.last), method: :post)
   end
 
   def record_not_found
     redirect_to entries_url
   end
+
+  def search_entries
+    Entry.search(params[:search]).page
+  end
+
+  def sort_entries
+    Entry.order(sort_column + ' ' + sort_direction).page
+  end
+
   def sort_column
-    Entry.column_names.include?(params[:sort]) ? params[:sort] : "japanische_umschrift"
+    Entry.column_names.include?(params[:sort]) ? params[:sort] : 'japanische_umschrift'
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    ['asc', 'desc'].include?(params[:direction]) ? params[:direction] : 'asc'
   end
+
 end
