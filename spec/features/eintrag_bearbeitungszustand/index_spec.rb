474807fa3126d9aa4dcb7c5bdca7bfa2522ed73b
max <mmaintz@posteo.de>
2017-11-28 20:31:59 +0100

    improve a spec

--- spec/features/eintrag_bearbeitungszustand/index_spec.rb
+++ spec/features/eintrag_bearbeitungszustand/index_spec.rb
@@ -5,27 +5,41 @@ describe 'displays correct state of editing of entries of index' do
   let!(:formatted_entry) { FactoryBot.build(:entry, bearbeitungsstand: 'formatiert') }
   let!(:unformatted_entry) { FactoryBot.build(:entry, bearbeitungsstand: 'unformatiert') }
   let!(:unprocessed_entry) { FactoryBot.build(:entry, bearbeitungsstand: 'unbearbeitet') }
+  let!(:deprecated_syntax_entry) { FactoryBot.build(:entry, bearbeitungsstand: 'Code veraltet') }
 
   before do
     login_as_user(admin)
   end
-  xdescribe 'admin visits entries#index' do
-    context 'when unprocessed' do
-      it 'entries are labeled accordingly' do
+  describe 'admin visits entries#index' do
+    context 'unprocessed entries' do
+      it 'are labeled accordingly' do
         unprocessed_entry.save
         visit entries_path
-        expect(page).to have_content('unbearbeitet')
-        expect(page).not_to have_content('unformatiert')
-        expect(page).not_to have_content('formatiert')
+        expect(page.find('span.label-danger').text).to eq('unbearbeitet')
+        expect(all('span.label-success').count).to eq(0)
+        expect(all('span.label-info').count).to eq(0)
+        expect(all('span.label-warning').count).to eq(0)
       end
     end
-    context 'when formatted' do
-      it 'entries are labeled accordingly' do
+    context 'formatted entries' do
+      it 'are labeled accordingly' do
         formatted_entry.save
         visit entries_path
-        # unbearbeitet is the name of a link in the sidebar menu now so it
-        # appears anyways and I have to be more specific
-        expect(page).not_to have_content('unbearbeitet')
+        expect(page.find('span.label-success').text).to eq('formatiert')
+      end
+    end
+    context 'unformatted entries' do
+      it 'are labeled accordingly' do
+        unformatted_entry.save
+        visit entries_path
+        expect(page.find('span.label-info').text).to eq('unformatiert')
+      end
+    end
+    context 'Deprecated syntax entries' do
+      it 'are labeled accordingly' do
+        deprecated_syntax_entry.save
+        visit entries_path
+        expect(page.find('span.label-warning').text).to eq('Code veraltet')
       end
     end
   end
