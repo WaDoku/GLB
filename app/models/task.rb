3f7c28d22f5178fddde578f9a64e56ebc52a33b8
max <mmaintz@posteo.de>
2017-12-11 11:12:13 +0100

    Add Validation to Task-Model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
+  validates :assigned_from_user, :assigned_to_user, :assigned_at_date, :assigned_to_date, presence: true
+
   def entry
-    Entry.find(self.assigned_entry)
+    Entry.find(assigned_entry)
   end
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
-    assigned_from_user 1
-    assigned_to_user ""
-    assigned_at_date "2017-12-08"
-    assigned_to_date "2017-12-08"
-    assigned_entry 1
+    assigned_from_user { create(:admin).id }
+    assigned_to_user { create(:editor).id }
+    assigned_at_date Date.today
+    assigned_to_date Date.today + 3.month
+    assigned_entry { create(:entry).id }
   end
 end
