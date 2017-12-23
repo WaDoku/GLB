e9d8934edb1becedf34224b81d6b4f0c0be632b4
max <mmaintz@posteo.de>
2017-12-23 12:03:16 +0100

    Add specs for TaskHelper

--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -1,15 +1,33 @@
-require 'rails_helper'
+require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TasksHelper. For example:
-#
-# describe TasksHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TasksHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:assigned_entry) { create(:entry) }
+  let(:task_creator) { create(:admin) }
+  let(:assigned_user) { create(:editor) }
+  let(:task) { create(:task) }
+
+  describe 'entry' do
+    it 'returns assigned entry' do
+      task.update(assigned_entry: assigned_entry.id)
+      expect(task.entry).to eq(assigned_entry)
+    end
+  end
+  describe 'name_of_assigned_user' do
+    it 'returns name of assigned user' do
+      task.update(assigned_to_user: assigned_user.id)
+      expect(task.name_of_assigned_user).to eq(assigned_user.name)
+    end
+  end
+  describe 'email_of_assigned_user' do
+    it 'returns name of assigned user' do
+      task.update(assigned_to_user: assigned_user.id)
+      expect(task.email_of_assigned_user).to eq(assigned_user.email)
+    end
+  end
+  describe 'name_of_task_creator' do
+    it 'returns name of creator of task' do
+      task.update(assigned_from_user: task_creator.id)
+      expect(task.name_of_task_creator).to eq(task_creator.name)
+    end
+  end
 end
