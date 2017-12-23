f73c85bdebce83f80d0d28ddff1eb6c6564ad4fa
max <mmaintz@posteo.de>
2017-12-23 20:11:27 +0100

    Fix user_entries_pagination_spec

--- spec/features/user_entries/user_entries_pagination_spec.rb
+++ spec/features/user_entries/user_entries_pagination_spec.rb
@@ -6,13 +6,13 @@ describe 'user_entries pagination' do
   describe 'admin visits user entries' do
     before do
       login_as_user(admin)
-      100.times { create(:entry) }
+      100.times { create(:entry, user_id: admin.id) }
       visit user_entries_path(admin.id)
     end
-    xit 'sees pagination navigation' do
+    it 'sees pagination navigation' do
       expect(page.all('ul.pagination').count).to eq(2)
     end
-    xit 'is able to navigate between pages' do
+    it 'is able to navigate between pages' do
       expect(page).to have_link('Letzte')
       first(:link, 'Letzte').click
       expect(page).not_to have_link('Letzte')
