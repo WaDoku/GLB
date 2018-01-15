8ced8066f35f4454ce7b5ebb2bfcf1d0457584ca
max <mmaintz@posteo.de>
2018-01-15 12:48:11 +0100

    Fix a Spec

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe "PasswordResets" do
-  xit "emails user when requesting password reset" do
+  it "emails user when requesting password reset" do
     user = FactoryBot.create(:author)
     visit new_user_session_path
     click_link "Passwort vergessen?"
     fill_in "E-Mail", :with => user.email
     click_button "Anleitung schicken, um mein Passwort zurückzusetzen"
-    expect(ActionMailer::Base.deliveries[0].to).to include(user.email)
+    expect(ActionMailer::Base.deliveries.last.to).to eq([user.email])
   end
 
 end
