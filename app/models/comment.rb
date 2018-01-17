3bf8f312a02bce2931270145bbd68b6875970659
max <mmaintz@posteo.de>
2018-01-17 19:26:18 +0100

    Use a scope for split comments between sections

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :entry
   validates :comment, :user_id, :entry_id, presence: true
+  scope :split_first, -> { order("updated_at DESC").limit(1) }
+  scope :split_tale, -> { order("updated_at DESC").offset(1) }
 end
