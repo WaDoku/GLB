09b9d78195d5e76d264c945b986cdce27299df71
max <mmaintz@posteo.de>
2018-01-18 09:59:37 +0100

    Refactor versions_controller

--- app/controllers/entry_versions_controller.rb
+++ app/controllers/entry_versions_controller.rb
@@ -2,21 +2,11 @@ class EntryVersionsController < ApplicationController
   before_action :find_current_entry, only: [:index, :show]
 
   def index
-    @entry_versions_all = []
-    if @entry.versions.size > 1
-      1.upto(@entry.versions.size) do |count_me|
-        if @entry.versions[- count_me].event == 'update'
-          @entry_versions_all << @entry.versions[- count_me]
-        end
-      end
-    end
+    @versions = @entry.versions.all
   end
 
   def show
-    @version_number = params[:id].to_i
-    entry_papertrail_version = @entry.versions[- @version_number]
-    @entry_created_at = entry_papertrail_version.created_at
-    @entry_papertrail_version_reified = entry_papertrail_version.reify
+    @version = @entry.versions.find(params[:id]).reify
   end
 
   private
@@ -24,4 +14,8 @@ class EntryVersionsController < ApplicationController
   def find_current_entry
     @entry = Entry.find(params[:entry_id])
   end
+
+  # def version_params
+  #   params.require(:version).permit(:version_id)
+  # end
 end
