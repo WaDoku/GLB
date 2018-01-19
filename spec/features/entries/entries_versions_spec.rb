8bca261a639699542fcae9f4a992eb04515bff74
max <mmaintz@posteo.de>
2018-01-19 11:09:49 +0100

    Fix two feature Specs

--- spec/features/entries/entries_versions_spec.rb
+++ spec/features/entries/entries_versions_spec.rb
@@ -17,24 +17,22 @@ describe 'entry_versions management' do
         fill_in 'entry_kennzahl', with: '1:3'
         click_button('Speichern')
       end
-      it 'visits version index and sees two versions of entry' do
+      it 'visits version index and sees current- plus two versions of entry' do
         visit entry_versions_path(entry)
-        expect(page).to have_css('h6', count: 2)
+        expect(page.all('tr > td').count).to eq(12)
       end
       it 'visits current version and sees respective changes' do
         visit entry_versions_path(entry)
-        click_link('Aktuelle Version')
+        click_link('Anzeigen', match: :first)
         expect(page).to have_content('1:3')
       end
       it 'visits first version and sees respective changes' do
-        visit entry_versions_path(entry)
-        click_link('Version: 1')
-        expect(page).to have_content('1:2')
+        visit entry_version_path(entry.versions[1].item_id, entry.versions[1].id)
+        expect(page).to have_content('1:1')
       end
       it 'visits second version and sees respective changes' do
-        visit entry_versions_path(entry)
-        click_link('Version: 2')
-        expect(page).to have_content('1:1')
+        visit entry_version_path(entry.versions[2].item_id, entry.versions[2].id)
+        expect(page).to have_content('1:2')
       end
     end
   end
