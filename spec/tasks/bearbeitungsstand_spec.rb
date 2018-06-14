354f41b6069963dc39d7a571f798c0c021fdcc38
max <mmaintz@posteo.de>
2018-06-14 16:27:43 +0200

    Improve task/bearbeitungsstand_spec

--- spec/tasks/bearbeitungsstand_spec.rb
+++ spec/tasks/bearbeitungsstand_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'rake db:label_bearbeitungsstand', type: :task do
-  let(:rake_task_user) { FactoryBot.build(:admin, email: 'rake@user.com') }
-  let(:entry) { FactoryBot.build(:entry) }
-  let(:unprocessed_entry) { FactoryBot.build(:unprocessed_entry, bearbeitungsstand: nil) }
-  let(:formatted_entry) { FactoryBot.build(:formatted_entry, bearbeitungsstand: nil) }
-  let(:unformatted_entry) { FactoryBot.build(:unformatted_entry, bearbeitungsstand: nil) }
-  let(:deprecated_syntax_entry) { FactoryBot.build(:deprecated_syntax_entry, bearbeitungsstand: nil) }
+  let(:entry)                   { build(:entry) }
+  let(:unprocessed_entry)       { build(:unprocessed_entry, bearbeitungsstand: nil) }
+  let(:formatted_entry)         { build(:formatted_entry, bearbeitungsstand: nil) }
+  let(:unformatted_entry)       { build(:unformatted_entry, bearbeitungsstand: nil) }
+  let(:deprecated_syntax_entry) { build(:deprecated_syntax_entry, bearbeitungsstand: nil) }
+  let(:already_labeled_entry)   { build(:entry, bearbeitungsstand: 'formatiert', uebersetzung: nil) }
 
   context 'general' do
     it 'preloads the Rails environment' do
@@ -22,32 +22,38 @@ describe 'rake db:label_bearbeitungsstand', type: :task do
     it 'labels an unprocessed entry as unbearbeitet' do
       unprocessed_entry.save
       task.execute
-      expect(Entry.last.bearbeitungsstand).to eq('unbearbeitet')
+      unprocessed_entry.reload
+      expect(unprocessed_entry.bearbeitungsstand).to eq('unbearbeitet')
     end
 
     it 'labels a formatted entry as formatiert' do
       formatted_entry.save
       task.execute
-      expect(Entry.last.bearbeitungsstand).to eq('formatiert')
+      formatted_entry.reload
+      expect(formatted_entry.bearbeitungsstand).to eq('formatiert')
     end
 
     it 'labels a unformatted entry as unformatiert' do
       unformatted_entry.save
       task.execute
-      expect(Entry.last.bearbeitungsstand).to eq('unformatiert')
+      unformatted_entry.reload
+      expect(unformatted_entry.bearbeitungsstand).to eq('unformatiert')
     end
 
     it 'labels an entry that contains deprecated syntax as Code veraltet' do
       deprecated_syntax_entry.save
       task.execute
-      expect(Entry.last.bearbeitungsstand).to eq('Code veraltet')
+      deprecated_syntax_entry.reload
+      expect(deprecated_syntax_entry.bearbeitungsstand).to eq('Code veraltet')
     end
 
     it 'does not overwrites labels that where already set' do
-      entry.update(bearbeitungsstand: 'formatiert', uebersetzung: nil)
+      already_labeled_entry.save
+      expect(already_labeled_entry.bearbeitungsstand).to eq('formatiert')
       task.execute
-      expect(Entry.last.bearbeitungsstand).to eq('formatiert')
-      expect(Entry.last.bearbeitungsstand).not_to eq('unbearbeitet')
+      already_labeled_entry.reload
+      expect(already_labeled_entry.bearbeitungsstand).to eq('formatiert')
+      expect(already_labeled_entry.bearbeitungsstand).not_to eq('unbearbeitet')
     end
   end
 end
