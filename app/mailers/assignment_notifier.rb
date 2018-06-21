84e647ab8970c1b3c4fdfc02f8ba7314dcce0d61
max <mmaintz@posteo.de>
2018-06-21 16:25:27 +0200

    Refactor assignment_notifier

--- app/mailers/assignment_notifier.rb
+++ app/mailers/assignment_notifier.rb
@@ -1,44 +1,28 @@
 class AssignmentNotifier < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.assignment_notifier.assignment_assigned.subject
-  #
   def create(assignment)
     @assignment = assignment
-    subject = "Zuweisung des Eintrags #{assignment.entry.japanische_umschrift} (#{assignment.entry.kennzahl})"
+    subject = I18n.t('assignment_notifier.create.subject', japanische_umschrift: assignment.entry.japanische_umschrift, kennzahl: assignment.entry.kennzahl)
 
     mail to: User.find(assignment.recipient_id).email, subject: subject
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.assignment_notifier.assignment_done.subject
-  #
   def done(assignment)
     @assignment = assignment
-    subject = "Eintrag #{assignment.entry.japanische_umschrift} (#{assignment.entry.kennzahl}) erledigt"
+    subject = I18n.t('assignment_notifier.done.subject', japanische_umschrift: assignment.entry.japanische_umschrift, kennzahl: assignment.entry.kennzahl)
 
     mail to: User.find(assignment.creator_id).email, subject: subject
   end
 
   def reminder(assignment)
     @assignment = assignment
-    subject = "Überschreitung des Bearbeitungszeitraums für Eintrag #{assignment.entry.japanische_umschrift} (#{assignment.entry.kennzahl})"
+    subject = I18n.t('assignment_notifier.reminder.subject', japanische_umschrift: assignment.entry.japanische_umschrift, kennzahl: assignment.entry.kennzahl)
 
     mail to: User.find(assignment.recipient_id).email, subject: subject
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.assignment_notifier.reminder.subject
-  #
   def expired(assignment)
     @assignment = assignment
-    subject = "Rücknahme des Bearbeitungsrechts für Eintrag #{assignment.entry.japanische_umschrift} (#{assignment.entry.kennzahl})"
+    subject = I18n.t('assignment_notifier.expired.subject', japanische_umschrift: assignment.entry.japanische_umschrift, kennzahl: assignment.entry.kennzahl)
 
     mail to: User.find(assignment.recipient_id).email, subject: subject
     mail cc: User.find(assignment.creator_id).email, subject: subject
